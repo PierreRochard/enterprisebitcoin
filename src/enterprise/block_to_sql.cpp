36111c870b696daf9c63dbacba901c80b25f546d
Pierre Rochard <pierre@rochard.org>
2021-11-22 17:05:30 -0600

    Fix type issue

--- src/enterprise/block_to_sql.cpp
+++ src/enterprise/block_to_sql.cpp
@@ -74,7 +74,7 @@ BlockToSql::BlockToSql(const CBlockIndex block_index, const CBlock block) : m_bl
 
     std::map<double, accumulator_t> accumulators;
     for ( double n = 0.01; n < 1 ; n += 0.01 ) {
-        accumulators.insert(std::make_pair(n, accumulator_t(quantile_probability = n)))
+        accumulators.insert(std::make_pair(n, accumulator_t(quantile_probability = n)));
     }
 
     for (std::size_t transaction_index = 0; transaction_index < m_block.vtx.size(); ++transaction_index) {
