658858f3cd02363d51f7ec06aa0ba7a27abcfeb9
Pierre Rochard <pierre@rochard.org>
2021-11-30 20:40:03 -0600

    Add script type query

--- src/enterprise/fee_rate_aggregation_query.sql
+++ src/enterprise/fee_rate_aggregation_query.sql
@@ -1,15 +1,14 @@
-SELECT
-       daydate,
-       count(height) as heights,
-       sum(total_fees) as total_fees,
-       sum(total_weight) as total_weight,
-       percentile_cont((select array_agg(s) from generate_series(0, 1, 0.1) as s)) within group (order by fee_rate)::int[],
+SELECT daydate,
+       count(height)        as heights,
+       sum(total_fees)      as total_fees,
+       sum(total_weight)    as total_weight,
+       fee_rate             as fee_rate,
        sum(fee_rate_weight) as fee_rate_weight
 
 FROM (
          SELECT height,
                 outputs_count,
-                median_time::date as daydate,
+                median_time::date                               as daydate,
                 total_fees,
                 total_weight,
                 (jsonb_array_elements(fee_rates) ->> 0)::bigint as fee_rate,
@@ -20,5 +19,4 @@ FROM (
          ORDER BY median_time DESC
      ) as b
 WHERE fee_rate > 0
-GROUP BY daydate
-LIMIT 10;
+GROUP BY daydate, fee_rate;
